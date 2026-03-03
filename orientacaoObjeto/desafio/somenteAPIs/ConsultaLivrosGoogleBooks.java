2b8638ffa52ff828e775d6635d5540d559c8c86d
luizcrisantoo <luiz.crisanto@gmail.com>
2026-03-03 10:56:31 -0300

    Trabalhando com APIs

---
+++ orientacaoObjeto/desafio/somenteAPIs/ConsultaLivrosGoogleBooks.java
@@ -0,0 +1,26 @@
+package orientacaoObjeto.desafio.somenteAPIs;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class ConsultaLivrosGoogleBooks {
+    public static void main(String[] args) throws IOException, InterruptedException {
+      Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um livro para buscar: ");
+        var tituloLivro = leitura.nextLine();
+        
+        String chave = "&apikeyAIzaSyAurD7SkwEXGr-BQ_mvpvhhHF6m10KbysA";
+        String endereco = "https://www.googleapis.com/books/v1/volumes?q=" + tituloLivro + "&key=" + chave;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
+
