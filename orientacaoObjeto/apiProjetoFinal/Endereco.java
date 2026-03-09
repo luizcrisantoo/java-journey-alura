3a0bfd9e04c938b7087f3aceffd929044590ae47
luizcrisantoo <luiz.crisanto@gmail.com>
2026-03-09 15:50:18 -0300

    API Busca Cep

---
+++ orientacaoObjeto/apiProjetoFinal/Endereco.java
@@ -0,0 +1,6 @@
+package orientacaoObjeto.apiProjetoFinal;
+
+public record Endereco(String cep, String logradouro,
+                      String complemento, String bairro,
+                      String localidade, String uf) {
+}
